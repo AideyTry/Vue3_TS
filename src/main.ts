62e0d1bacb8ef12624650b3dfec52c5638644d88
Aiden <jason_dlb@sina.cn>
2022-10-06 10:14:52 +0800

    chore: config eslint and commitlint

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: Aiden(戴林波)
+ * @Date: 2022-10-05 16:39:50
+ * @LastEditTime: 2022-10-05 22:45:24
+ * @LastEditors: Aiden(戴林波)
+ * @Description:
+ * @Email: jason_dlb@sina.cn
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 
