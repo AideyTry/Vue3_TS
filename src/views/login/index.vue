b8acd9b1055471fcb6b1f534f583802f8ba747de
Aiden <jason_dlb@sina.cn>
2022-10-16 22:44:37 +0800

    feat: login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,29 +1,85 @@
 <template>
-  <div>
-    login
-    <h1>{{ a }}</h1>
-    <h2>{{ $store.state.count }}</h2>
+  <div class="login-container">
+    <el-form
+      class="login-form"
+      :rules="rules"
+      ref="form"
+      :model="user"
+      size="medium"
+      @submit.prevent="handleSubmit"
+    >
+      <div class="login-form__header">
+        <img
+          class="login-logo"
+          src="@/assets/login_logo.png"
+          alt="蔓草"
+        >
+      </div>
+      <el-form-item prop="account">
+        <el-input
+          v-model="user.account"
+          placeholder="请输入用户名"
+        />
+      </el-form-item>
+      <el-form-item prop="pwd">
+        <el-input
+          v-model="user.pwd"
+          type="password"
+          placeholder="请输入密码"
+        />
+      </el-form-item>
+      <el-form-item prop="imgcode">
+        <div class="imgcode-wrap">
+          <el-input
+            v-model="user.imgcode"
+            placeholder="请输入验证码"
+          />
+          <img
+            class="imgcode"
+            alt="验证码"
+            src="https://shop.fed.lagounews.com/api/admin/captcha_pro"
+          >
+        </div>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          class="submit-button"
+          type="primary"
+          :loading="loading"
+          native-type="submit"
+        >
+          登录
+        </el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { useStore } from '../../store'
-import { getLoginInfo } from '@/api/common'
-import type { LoginInfo } from '@/api/types/common'
 
-const store = useStore()
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
 
-console.log('store.state=', store.state.count)
-const a = ref(0)
-const list = ref<LoginInfo['slide']>([])
-
-onMounted(() => {
-  getLoginInfo().then(data => {
-    console.log('data==', data)
-    list.value = data.slide
-    console.log('list.value===', list.value)
-  })
+const user = reactive({
+  account: 'admin',
+  pwd: '123456',
+  imgcode: ''
 })
+const loading = ref(false)
+const rules = ref({
+  account: [
+    { required: true, message: '请输入账号', trigger: 'change' }
+  ],
+  pwd: [
+    { required: true, message: '请输入密码', trigger: 'change' }
+  ],
+  imgcode: [
+    { required: true, message: '请输入验证码', trigger: 'change' }
+  ]
+})
+
+const handleSubmit = async () => {
+  console.log('handleSubmit')
+}
+
 </script>
 <script lang="ts">
 export default {
@@ -31,4 +87,52 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.login-container {
+  min-width: 400px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #2d3a4b;
+}
+
+.login-form {
+  padding: 30px;
+  border-radius: 6px;
+  background: #fff;
+  min-width: 350px;
+  .login-form__header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 30px;
+  }
+
+  .el-form-item:last-child {
+    margin-bottom: 0;
+  }
+
+  .login__form-title {
+    display: flex;
+    justify-content: center;
+    color: #fff;
+  }
+
+  .submit-button {
+    width: 100%;
+  }
+
+  .login-logo {
+    width: 271px;
+    height: 74px;
+  }
+  .imgcode-wrap {
+    display: flex;
+    align-items: center;
+    .imgcode {
+      height: 37px;
+    }
+  }
+}
+</style>
